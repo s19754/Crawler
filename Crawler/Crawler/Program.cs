20e642e43d0f047586a832923b26dfeda4aa57e5
Tomasz Wojtkowski <s19754@pjwstk.edu.pl>
2022-03-18 17:03:35 +0100

    gotowe

---
+++ Crawler/Crawler/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Crawler
+{
+    class Program
+    {
+        public async static Task Main(string[] args)
+        {
+            if (args.Length < 1) throw new ArgumentNullException("Parameter 0 cannot be null.");
+            var websiteUrl = args[0];
+
+            Regex rgx = new Regex("^(http|https|ftp)\\://([a-zA-Z0-9\\.\\-]+(\\:[a-zA-Z0-9\\.&amp;%\\$\\-]+)*@)*((25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[1-9])\\.(25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[1-9]|0)\\.(25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[1-9]|0)\\.(25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[0-9])|localhost|([a-zA-Z0-9\\-]+\\.)*[a-zA-Z0-9\\-]+\\.(com|edu|gov|int|mil|net|org|biz|arpa|info|name|pro|aero|coop|museum|[a-zA-Z]{2}))(\\:[0-9]+)*(/($|[a-zA-Z0-9\\.\\,\\?\\'\\\\+&amp;%\\$#\\=~_\\-]+))*$");
+
+            Match match = rgx.Match(args[0]);
+            if (!match.Success) throw new ArgumentException("URL not valid: " + args[0]);
+
+            var httpClient = new HttpClient();
+
+            try
+            {
+
+                var response = await httpClient.GetAsync(websiteUrl);
+
+                var content = await response.Content.ReadAsStringAsync();
+
+
+
+                var regex = new Regex(@"[a-zA-Z0-9+._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9_-]+");
+                MatchCollection matchCollection = regex.Matches(content);
+
+                var set = new HashSet<string>();
+
+                if (Convert.ToBoolean(matchCollection.Count))
+                {
+                    string[] fAddr = matchCollection.Cast<Match>().Select(x => x.Value).ToArray();
+                    Array.ForEach(fAddr.Distinct().ToArray(), val => Console.WriteLine(val));
+                }
+                else
+                {
+                    Console.WriteLine("Nie znaleziono adresów email.");
+                }
+            }
+            catch (HttpRequestException ex)
+            {
+                Console.WriteLine("Błąd w czasie pobierania strony");
+            }
+            finally
+            {
+                httpClient.Dispose();
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Net.Http;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
@@ -9,24 +10,34 @@ namespace Crawler
     {
         public async static Task Main(string[] args)
         {
+            if (args.Length < 1) throw new ArgumentNullException();
             var websiteUrl = args[0];
-            Console.WriteLine(websiteUrl);
+           
 
             var httpClient = new HttpClient();
 
-            var response = httpClient.GetAsync(websiteUrl);
+            var response = await httpClient.GetAsync(websiteUrl);
             Console.WriteLine(response);
+
+
             var content = await response.Content.ReadAsStringAsync();
             Console.WriteLine(content);
 
 
 
-            var regex = new Regex(@"");
+            var regex = new Regex(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$");
             var matchCollection = regex.Matches(content);
+
+            var set = new HashSet<string>();
 
             foreach (var item in matchCollection)
             {
+                set.Add(item.ToString());
+            }
 
+            foreach (var item in matchCollection)
+            {
+                Console.WriteLine(item);
             }
         }
     }
